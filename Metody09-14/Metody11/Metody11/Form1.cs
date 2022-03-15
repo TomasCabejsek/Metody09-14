578840fe5a9fd172d90ddc9cfa64c56f6660b070
cabejsekt.04 <cabejsekt.04@A321-15>
2022-03-15 09:38:46 +0100

    Metody11

--- Metody09-14/Metody11/Metody11/Form1.cs
+++ Metody09-14/Metody11/Metody11/Form1.cs
@@ -16,5 +16,37 @@ namespace Metody11
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private int PocetSlov(string retezec, out string bezCislic)
+        {
+            char[] oddelovace = { ' ' };
+            string[] poleSlov = retezec.Split(oddelovace, StringSplitOptions.RemoveEmptyEntries);
+            int i = 0;
+            while (i < retezec.Length)
+            {
+                if (Char.IsDigit(retezec[i]))
+                {
+                    retezec = retezec.Remove(i, 1);
+                }
+                else --i;
+                ++i;
+            }
+            bezCislic = retezec;
+            return poleSlov.Length;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string bezCislic;
+            PocetSlov(s, out bezCislic);
+            MessageBox.Show("Pocet slov je " + PocetSlov(s, out bezCislic) + "Veta vypada takhle bez cislic " + bezCislic);
+
+        }
     }
 }
