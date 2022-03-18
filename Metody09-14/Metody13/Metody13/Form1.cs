4698fbca80f3092854dd03ea25e5afbe06d5471a
cabejsekt.04 <cabejsekt.04@A321-15>
2022-03-18 11:19:23 +0100

    Metody 13 a14

--- Metody09-14/Metody13/Metody13/Form1.cs
+++ Metody09-14/Metody13/Metody13/Form1.cs
@@ -16,5 +16,51 @@ namespace Metody13
         {
             InitializeComponent();
         }
+
+        private bool JeAlfanum(string retezec, out int pocetMalychPismen, out int pocetVelkychPismen, out int pocetJinychZnaku)
+        {
+            bool jeAlfanumericky = true;
+            pocetJinychZnaku = 0;
+            pocetMalychPismen = 0;
+            pocetVelkychPismen = 0;
+            for (int i = 0; i < retezec.Length; i++)
+            {
+                if (Char.IsLetterOrDigit(retezec[i]))
+                {
+                    if (Char.IsUpper(retezec[i]))
+                    {
+                        ++pocetVelkychPismen;
+                    }
+                    else if (Char.IsLower(retezec[i]))
+                    {
+                        ++pocetMalychPismen;
+                    }
+                }
+                else
+                {
+                    ++pocetJinychZnaku;
+                    jeAlfanumericky = false;
+                }
+            }
+            return jeAlfanumericky;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            int pocetMalPis = 0;
+            int pocetVelPis = 0;
+            int pocetJinych = 0;
+
+            if (JeAlfanum(s, out pocetMalPis, out pocetVelPis, out pocetJinych))
+            {
+                MessageBox.Show("Ano retezec je alfanumericky obsahuje jen cislice a pismena \nPocet malych pismen je " + pocetMalPis + "\nPocet velkych pismen je " + pocetVelPis);
+            }
+            else
+            {
+                MessageBox.Show("Ne retezec neni alfanumericky obsahuje cislice, pismena a jine znaky \nPocet malych pismen je " + pocetMalPis + "\nPocet velkych pismen je " + pocetVelPis + "\nPocet jinych znaku je " + pocetJinych);
+            }
+        }
     }
 }
--- Metody09-14/Metody14/Metody14/Form1.cs
+++ Metody09-14/Metody14/Metody14/Form1.cs
@@ -16,5 +16,65 @@ namespace Metody14
         {
             InitializeComponent();
         }
+
+        private bool Identicke(string retezec, string retezecdva, out int pocetLisicichSePozic, out int indexPrvniOdlisnosti)
+        {
+            bool identika = true;
+
+            indexPrvniOdlisnosti = -1;
+
+            int delka;
+            if (retezec.Length > retezecdva.Length)
+            {
+                delka = retezecdva.Length;
+                pocetLisicichSePozic = retezec.Length - retezecdva.Length;
+            }
+            else
+            {
+                delka = retezec.Length;
+                pocetLisicichSePozic = retezecdva.Length - retezec.Length;
+            }
+
+
+            for (int i = 0; i < delka; i++)
+            {
+                if (retezec[i] != retezecdva[i])
+                {
+                    identika = false;
+                    ++pocetLisicichSePozic;
+                    if (indexPrvniOdlisnosti == -1)
+                    {
+                        indexPrvniOdlisnosti = i;
+                    }
+                }
+                
+            }
+            if (indexPrvniOdlisnosti == -1)
+            {
+                indexPrvniOdlisnosti = delka;
+            }
+            if (pocetLisicichSePozic > 0)
+            {
+                identika = false;
+            }
+            return identika;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            string ss = textBox2.Text;
+            int indexOdl = 0;
+            int pocetOdl = 0;
+
+            if (Identicke(s, ss, out pocetOdl, out indexOdl))
+            {
+                MessageBox.Show("Retezce jsou identicke");
+            }
+            else
+            {
+                MessageBox.Show("Retezce jsou odlisne index prvni odlisnosti je " + indexOdl + " Pocet odlisnych je " + pocetOdl);
+            }
+        }
     }
 }
